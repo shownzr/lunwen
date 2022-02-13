73c5f591877a77bee046ad8ca677cbbcf9f7acbb
XJL-BYQVIUJLN7Y\shouzhenren <1146660681@qq.com>
2022-02-13 17:30:48 +0800

    2022.02.13

---
+++ learn/js/sxsx.js
@@ -0,0 +1,8 @@
+var name="ss";
+var age=18;
+var person={
+  name,
+  age,
+};
+
+console.log(person)
---
+++ learn/js/模板字符串.js
@@ -0,0 +1,12 @@
+var person ={
+  name:"ss",
+  age:18,
+}
+
+var str1 = "my name is " + person.name + "\nmy age is "+person.age;
+
+var str2 = `my name is ${person.name}
+my age is ${person.age}`
+
+console.log(str1)
+console.log(str2)
